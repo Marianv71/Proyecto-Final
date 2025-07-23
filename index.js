fbf31d1f4f7d9438aa6f9b3cce1e99e843e55821
Marian Villavicencio <mariana_villavicencio@hotmail.com>
2025-07-23 16:33:45 -0300

    Integración con Firebase y validación con JsonWebtoken (Bearer token)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Importo las variables de entorno
 import "dotenv/config";
-
 import express from "express";
-
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({message: "API REST del Proyecto Final, funcionando!"})
@@ -11,6 +11,9 @@ app.get("/", (req, res) => {
 
 import productosRouter from "./src/routes/productos.router.js";
 app.use("/api", productosRouter);
+
+import authRouter from "./src/routes/auth.router.js";
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 3001;
 
--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -3,8 +3,10 @@ const router = Router();
 
 import { getAllProductos, getProductoByID } from "../controllers/productos.controller.js";
 
+import { auth } from "../middlewares/auth.middleware.js";
+
 router.get("/productos", getAllProductos);
-router.get("/productos/:id", getProductoByID);
+router.get("/productos/:id", auth, getProductoByID);
 
 
 export default router;
